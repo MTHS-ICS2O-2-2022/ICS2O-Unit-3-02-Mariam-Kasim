6af58bad4d68c0bd37d360c19d9c1e0a9e90cc20
Mariam-Kasim <124389357+Mariam-Kasim@users.noreply.github.com>
2023-04-04 23:30:57 +0000

    main

---
+++ go_app/main.go
@@ -0,0 +1,34 @@
+// Created by: Mariam Kasim
+// Created on: April 2023
+//
+// This program finds the volume of a pyramid
+
+package main
+
+import "fmt"
+
+func main() {
+	// This function finds the volume of a pyramid
+	var lenght float64
+	var width float64
+	var height float64
+
+
+	// input
+	fmt.Println("This program finds the volume of a pyramid.")
+	fmt.Println()
+	fmt.Print("Enter the length(mm): ")
+	fmt.Scanln(&length)
+	fmt.Print("Enter the width (mm): ")
+	fmt.Scanln(&width)
+	fmt.Print("Enter the height (mm): ")
+	fmt.Scanln(&height)
+
+	// process
+	volume = (length * width * height) / 3
+
+	// output
+	fmt.Println()
+	fmt.Println("The volume is: ", volume,"mm³".")
+	fmt.Println("\nDone.")
+}
--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
+
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mariam Kasim
+// Created on: Mar 2023
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates the volume of a pyramid
+ */
+function calculate() {
+  // input
+  const length = parseFloat(document.getElementById("length-number").value)
+  const width= parseFloat(document.getElementById("width-number").value)
+  const height = parseFloat(document.getElementById("height-number").value)
+
+  // process
+  const volume = length * width * height / 3
+
+  // output
+  document.getElementById('volume').innerHTML = 'volume is: ' + volume + ' mm³'
+}
